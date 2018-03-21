41fb90467eabc9e54d8c636ec786a5e829c456c5
WolfyD <wolfyden@gmail.com>
2018-03-21 13:42:41 +0100

    Add project files.

---
+++ starwars/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace starwars
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.rtb_Text = new System.Windows.Forms.RichTextBox();
+			this.btn_Exit = new System.Windows.Forms.Button();
+			this.btn_Start = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// rtb_Text
+			// 
+			this.rtb_Text.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.rtb_Text.Location = new System.Drawing.Point(12, 12);
+			this.rtb_Text.Name = "rtb_Text";
+			this.rtb_Text.Size = new System.Drawing.Size(253, 343);
+			this.rtb_Text.TabIndex = 0;
+			this.rtb_Text.Text = resources.GetString("rtb_Text.Text");
+			// 
+			// btn_Exit
+			// 
+			this.btn_Exit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.btn_Exit.Location = new System.Drawing.Point(12, 388);
+			this.btn_Exit.Name = "btn_Exit";
+			this.btn_Exit.Size = new System.Drawing.Size(135, 23);
+			this.btn_Exit.TabIndex = 1;
+			this.btn_Exit.Text = "Exit";
+			this.btn_Exit.UseVisualStyleBackColor = true;
+			// 
+			// btn_Start
+			// 
+			this.btn_Start.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.btn_Start.Location = new System.Drawing.Point(12, 361);
+			this.btn_Start.Name = "btn_Start";
+			this.btn_Start.Size = new System.Drawing.Size(135, 23);
+			this.btn_Start.TabIndex = 2;
+			this.btn_Start.Text = "Start";
+			this.btn_Start.UseVisualStyleBackColor = true;
+			this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel1.BackColor = System.Drawing.Color.Black;
+			this.panel1.Location = new System.Drawing.Point(271, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(574, 399);
+			this.panel1.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.label1.Location = new System.Drawing.Point(167, 361);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(48, 54);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "0 - 0\r\n0 - 0\r\n0 - 0";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(857, 421);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.btn_Start);
+			this.Controls.Add(this.btn_Exit);
+			this.Controls.Add(this.rtb_Text);
+			this.Name = "Form1";
+			this.Text = "Star Warsú";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox rtb_Text;
+		private System.Windows.Forms.Button btn_Exit;
+		private System.Windows.Forms.Button btn_Start;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ starwars/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace starwars
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			Load += Form1_Load;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			rtb_Text.SelectAll();
+			rtb_Text.SelectionAlignment = HorizontalAlignment.Center;
+			rtb_Text.DeselectAll();
+			panel1.BackgroundImageLayout = ImageLayout.Zoom;
+		}
+
+		private void btn_Start_Click(object sender, EventArgs e)
+		{
+			if(rtb_Text.Text != "") { loadImage(); }
+		}
+
+		public void loadImage()
+		{
+			Font fnt = new Font("Consolas", 22, FontStyle.Regular);
+			Size s = TextRenderer.MeasureText(rtb_Text.Text, fnt);
+
+			Bitmap bmp = new Bitmap(s.Width + 100, s.Height + 100);
+
+			System.Drawing.Drawing2D.Matrix matr = new System.Drawing.Drawing2D.Matrix();
+			matr.Shear(-0.5f, 0.3f);
+
+			string str = matr.Elements[0].ToString().PadLeft(4,' ') + " - " + matr.Elements[1] + "\r\n" + matr.Elements[2].ToString().PadLeft(4, ' ') + " - " + matr.Elements[3] + "\r\n" + matr.Elements[4].ToString().PadLeft(4, ' ') + " - " + matr.Elements[5];
+
+
+			using (Graphics g = Graphics.FromImage(bmp))
+			{
+				g.Transform = matr;
+				g.DrawString(rtb_Text.Text, fnt, Brushes.Yellow, new Point(bmp.Width / 2, 50), new StringFormat() { Alignment = StringAlignment.Center });
+			}
+
+			label1.Text = str;
+
+			panel1.BackgroundImage = bmp;
+		}
+	}
+}
